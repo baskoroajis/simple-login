e0cb53558b33a89b2eb8d752a84d85c2fb413acc
baskoro aji susetyo Koko <baskoroajisusetyo@gmail.com>
2019-09-23 16:45:22 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+// app.use(express.static(path.join(__dirname,"/../public")));
+app.use(express.static(__dirname + "/public"));
+
+app.listen(7777, function(){
+    console.log("Started listening on port", 7777);
+})
