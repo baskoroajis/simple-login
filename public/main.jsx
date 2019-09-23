937862c93f4de3a5fb1ba68fc407edd088e7d98a
baskoro aji susetyo Koko <baskoroajisusetyo@gmail.com>
2019-09-23 17:49:58 +0700

    add input handler method for email and password

--- public/main.jsx
+++ public/main.jsx
@@ -1,18 +1,46 @@
 class Signin extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.signIn = this.signIn.bind(this);
+        this.state = {
+          email:'',
+          password:''
+        };
+    }
+
+    handleEmailChange(e){
+        this.setState({email:e.target.value})
+    }
+    
+    handlePasswordChange(e){
+        this.setState({password:e.target.value})
+    }
+
+    signIn(){
+         alert('email is : ' +this.state.email+ ' password is : '+this.state.password);
+        // alert('lalalal')
+    }
+
     render(){
         return(
             <form className="form-signin">
                 <h2 className="form-signing-heading">Please sign in</h2>
                 <label for="inputEmail" className="sr-only">Email address</label>
-                <input type="email" id="inputEmail" className="form-controll" placeholder="Email address" required autoFocus></input>
+                <input type="email" id="inputEmail" className="form-controll" placeholder="Email address" required autoFocus onChange={this.handleEmailChange}></input>
                 <label for="inputPassword" className="sr-only"> Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                <button className="btn btn-lg btn-primary btn-block" type="button"> Sign in
+                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required onChange={this.handlePasswordChange}/>
+                <button className="btn btn-lg btn-primary btn-block" type="button" onClick={this.signIn}> Sign in
                 </button>
             </form>
         )
     }
+
 }
+
+
 
 ReactDOM.render(
     <Signin/>, 
